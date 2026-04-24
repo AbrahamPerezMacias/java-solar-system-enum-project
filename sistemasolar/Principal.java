f4ebca961389cb0fe429ca1546627f0a67f3f2fd
Abraham Pérez Macias <abraham.lcc@hotmail.com>
2026-04-24 11:26:36 -0600

    Create Principal.java

---
+++ sistemasolar/Principal.java
@@ -0,0 +1,12 @@
+// Principal.java
+package sistemasolar;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        TablaPlanetas tabla = new TablaPlanetas();
+        tabla.mostrarTabla();
+
+    }
+}
